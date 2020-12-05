fa47566497d21f5701f6ad61ecb0678afce90f16
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-12-05 02:57:38 +0100

    update the intent

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,17 +6,38 @@ router.get('/', (req: express.Request, res: express.Response) => {
   return res.json('Welcome to the emotions machine learning experience ')
 })
 
-router.post('/handleAndroidOptionRequest',(req: express.Request, res: express.Response) => {
-  const webHookRequest: any = req.body
-  return res.json({
-    "followupEventInput": {
-      "name": "android-event",
-      "languageCode": "en-US",
-      "parameters": {
-        "operatingSystem": "Android"
-      }
+router.post(
+  '/handleAndroidOptionRequest',
+  (req: express.Request, res: express.Response) => {
+    const webHookRequest: any = req.body
+    const intentName = webHookRequest.intent.displayName
+    switch (intentName) {
+      case 'OS Choice IOS':
+        return res.json({
+          followupEventInput: {
+            name: 'ios-event',
+            languageCode: 'en-US',
+            parameters: {
+              operatingSystem: 'IOS'
+            }
+          }
+        })
+      case 'OS Choice Android':
+        res.json({
+          followupEventInput: {
+            name: 'android-event',
+            languageCode: 'en-US',
+            parameters: {
+              operatingSystem: 'Android'
+            }
+          }
+        })
+        return
+      default:
+        return null
     }
-  })
-})
+    return
+  }
+)
 
 export default router
