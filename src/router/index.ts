6dd5d3d84e6ac4453a0e9cd8396a67471ce6a4e2
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-12-05 02:35:49 +0100

    dialogflow example

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,4 +6,19 @@ router.get('/', (req: express.Request, res: express.Response) => {
   return res.json('Welcome to the emotions machine learning experience ')
 })
 
+router.get('/handleAndroidOptionRequest',(req: express.Request, res: express.Response) => {
+  const webHookRequest: any = req.body
+  return res.json({
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Text response from webhook"
+          ]
+        }
+      }
+    ]
+  })
+})
+
 export default router
